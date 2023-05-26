f86129fb541cb3f043a8a2ffab500871b7413c66
DARSHANGOWDA2023 <134402307+DARSHANGOWDA2023@users.noreply.github.com>
2023-05-26 22:18:36 +0530

    Add files via upload

---
+++ flower_store/about.php
@@ -0,0 +1,188 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php @include 'header.php'; ?>
+
+<section class="heading">
+    <h3>ABOUT US</h3>
+    <p> <a href="home.php">home</a> / about </p>
+</section>
+
+<section class="about">
+
+    <div class="flex">
+
+        <div class="image">
+            <img src="images/about-img-1.png" alt="">
+        </div>
+
+        <div class="content">
+            <h3>why choose us?</h3>
+            <p>ONLY FRESH FLOWERS TO BE DISTRIBUTED, OLD STOCK ARE DISCARDED AT THE END OF THE DAY .</p>
+            <a href="shop.php" class="btn">shop now</a>
+        </div>
+
+    </div>
+
+    <div class="flex">
+
+        <div class="content">
+            <h3>what we provide?</h3>
+            <p>WE PROVIDE FRESH FLOWERS DIRECTLY FROM THE FARMERS END TO THE CUSTOMERS.</p>
+            <a href="contact.php" class="btn">contact us</a>
+        </div>
+
+        <div class="image">
+            <img src="images/about-img-2.jpg" alt="">
+        </div>
+
+    </div>
+
+    <div class="flex">
+
+        <div class="image">
+            <img src="images/about-img-3.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>who we are?</h3>
+            <p>WE ARE THE LARGEST FLOWER DISTRIBUTORS IN OUR LOCAL MARKET.</p>
+            <a href="#reviews" class="btn">clients reviews</a>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="reviews" id="reviews">
+
+    <h1 class="title">client's reviews</h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/pic-1.png" alt="">
+            <p>GREAT! GOOD DELIVERY PARTNER AVAILABLE . DELIVERY ON TIME</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>DARSHAN</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-2.png" alt="">
+            <p>FRESH FLOWERS DELIVERED,ON TIME !</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>ARUNA</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-3.png" alt="">
+            <p>GREAT PRICES ON BOUQUETS , OFFERS ARE AMAZING.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>MUKUL</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-4.png" alt="">
+            <p>NOSTALGIC FLOWERS.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>SMITHA</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-5.png" alt="">
+            <p>THESE FLOWERS REMIND ME OF SANDALWOOD.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>ABHINANDAN</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-6.png" alt="">
+            <p>WOW ! GREAT FLOWERS , I DIDN'T EXPECT THESE KIND OF BEAUTIFUL FLOWERS. THANK YOU.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>LAURA</h3>
+        </div>
+
+    </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<?php @include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ flower_store/footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="orders.php">My Orders</a>
+            <a href="cart.php">My Cart</a>
+        </div>
+
+        <div class="box">
+            <h3>CONTACT INFO.</h3>
+            <p> <i class="fas fa-phone"></i>9856589658</p>
+            <p> <i class="fas fa-phone"></i>8852124523</p>
+            <p> <i class="fas fa-envelope"></i> dn4343806@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> bangalore, india - 560098 </p>
+        </div>
+
+        <div class="box">
+            <h3>FOLLOW US ON</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>DARSHAN & SUHAS</span> </div>
+
+</section>
